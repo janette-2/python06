f4b19ead168544fcce16f7d436cf4f3c3c2a8580
Janette Rodriguez Ponce <janerodpon@gmail.com>
2026-04-24 21:33:18 +0200

    py6

---
+++ alchemy/grimoire/light_spellbook.py
@@ -0,0 +1,9 @@
+def light_spell_allowed_ingredients() -> list[str]:
+    return ["earth", "air", "fire", "water"]
+
+
+def light_spell_record(spell_name: str, ingredients: str) -> str:
+    validate_ing
+
+
+def validate_ingredients(ingredients: str):
